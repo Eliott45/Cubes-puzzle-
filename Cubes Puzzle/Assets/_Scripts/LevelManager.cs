d327f1dd78b4b4cee8423518d2be4d35970592a0
Eliott <eliott_nu@mail.ru>
2021-06-03 11:45:11 +0900

    Level change

---
+++ Cubes Puzzle/Assets/_Scripts/LevelManager.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _Scripts
+{
+    public class LevelManager : MonoBehaviour
+    {
+        private void FixedUpdate()
+        {
+            if (Input.GetKey(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+            if (Input.GetKey(KeyCode.Q))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player")) SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
